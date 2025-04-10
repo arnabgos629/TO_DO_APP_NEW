964ffeac0829d09fccfd285577057c2b9e1fbffc
Arnab Goswami <arnabgos629@gmail.com>
2025-04-11 00:23:32 +0530

    ok

--- backend/conn/conn.js
+++ backend/conn/conn.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const conn = async (req, res) => {
   try {
-    await mongoose.connect("use-your-mongo-db").then(() => {
+    await mongoose.connect("mongodb+srv://arnabgos629:BOPSshiaZFeUFgYt@cluster0.1giys.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(() => {
       console.log("Connected");
     });
   } catch (error) {
